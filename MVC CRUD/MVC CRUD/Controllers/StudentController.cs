b5c702ff95c65f4eb4aca0e8f47a53eda4430d0c
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2022-08-23 15:31:06 +0500

    Creation, View & Delete -- DONE

--- MVC CRUD/MVC CRUD/Controllers/StudentController.cs
+++ MVC CRUD/MVC CRUD/Controllers/StudentController.cs
@@ -1,8 +1,10 @@
-﻿using MVC_CRUD.Context;
+﻿using Microsoft.Ajax.Utilities;
+using MVC_CRUD.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 
 namespace MVC_CRUD.Controllers
@@ -20,16 +22,38 @@ namespace MVC_CRUD.Controllers
         [HttpPost]
         public ActionResult AddStudent(tbl_Student model)
         {
-            tbl_Student obj = new tbl_Student();
-            obj.Name = model.Name;
-            obj.Email = model.Email;    
-            obj.Fname = model.Fname;
-            obj.Mobile = model.Mobile;
-            obj.Description = model.Description;
-            dbObj.tbl_Student.Add(obj);
+            if (ModelState.IsValid)
+            {
+                tbl_Student obj = new tbl_Student();
+                obj.Name = model.Name;
+                obj.Email = model.Email;
+                obj.Fname = model.Fname;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+                dbObj.tbl_Student.Add(obj);
+                dbObj.SaveChanges();
+
+                ModelState.Clear();
+            }
+
+            int newid = 0;
+            return View("Student", new { newid });
+        }
+
+        public ActionResult StudentList()
+        {
+            var list = dbObj.tbl_Student.ToList();
+            return View(list);
+        }
+
+        public ActionResult StudentDelete(int id)
+        {
+            var res = dbObj.tbl_Student.Where(x => x.ID == id).First();
+            dbObj.tbl_Student.Remove(res);
             dbObj.SaveChanges();
 
-            return View("Student");
+            var list = dbObj.tbl_Student.ToList();
+            return View("StudentList", list);
         }
     }
 }
